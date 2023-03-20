3b12a77b1b7f9e6d2278eeb2fb379b682562d33a
Daniel Szemerey <account@szemerey.eu>
2023-03-20 17:01:09 +0100

    chore(Cleanup): We don't need all the project modules, just as a repository of datasets.

--- tests/test_energy.py
+++
@@ -1,7 +0,0 @@
-from load_data import load_data
-
-
-def test_industial_residual_load() -> None:
-    df = load_data("energy/industrial_pv_load")
-
-    assert len(df) == 105225, "Dataset length didn't match"
